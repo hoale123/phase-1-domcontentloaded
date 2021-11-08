05a7dfc7c64c159b5f30dacfcbebd3f76e0d9693
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-07 21:37:39 -0500

    commit tree cd482f2c8eefec57058e612926235f3b23738537

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    // detect when HTML page is load and doc is ready
+    updateDOM()
+})
+
+function updateDOM() {
+    document.getElementById( "text" )
+    .innerHTML = "This is really cool!"
+}
+
